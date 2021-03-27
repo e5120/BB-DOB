bb8ac853ba2e2648008f461c5610869ed56bce84
shimazu <e5120@gm.kochi-ct.jp>
2021-03-27 14:02:17 +0900

    add __init__

---
+++ bbdob/__init__.py
@@ -0,0 +1,7 @@
+from bbdob.objective_base import ObjectiveBase
+from bbdob.one_max import OneMax
+from bbdob.two_min import TwoMin
+from bbdob.four_peaks import FourPeaks
+from bbdob.deceptive_trap import DeceptiveTrap
+from bbdob.nk_landscape import NKLandscape
+from bbdob.w_model import WModel
